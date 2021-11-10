27aade97c059df2373428fd11e408515b969e2f4
Shubham Gupta <shubham0209@gmail.com>
2021-11-10 19:44:36 +0530

    Added sticky count dialog on long press addButton

--- app/src/main/java/org/smoke/sticky/tracker/MainActivity.kt
+++ app/src/main/java/org/smoke/sticky/tracker/MainActivity.kt
@@ -1,8 +1,12 @@
 package org.smoke.sticky.tracker
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import org.smoke.sticky.tracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +21,27 @@ class MainActivity : AppCompatActivity() {
             it.lifecycleOwner = this
         }
         setContentView(binding.root)
+        binding.addButton.setOnLongClickListener {
+            addStickyDialog()
+            true
+        }
+    }
+
+    private fun addStickyDialog() {
+        val input = EditText(this).apply {
+            hint = "Enter Cigs"
+            inputType = InputType.TYPE_CLASS_TEXT
+        }
+
+        AlertDialog.Builder(this)
+            .setTitle("Add multiple cigs")
+            .setMessage("How many cigs you smoked?")
+            .setView(input)
+            .setPositiveButton("Add") { _: DialogInterface, _: Int ->
+                input.text.toString().toFloatOrNull()?.let { trackerViewModel.addSticky(it) }
+            }
+            .create()
+            .show()
     }
 
 }
